ce941d9a206df24dc644509f8923ce3750652222
kiba-dev <kibamaster0301@gmail.com>
2022-11-10 21:31:16 -0800

    improve ui

--- src/pages/TopicDetailsPage/TopicDetailsPage.js
+++ src/pages/TopicDetailsPage/TopicDetailsPage.js
@@ -6,9 +6,11 @@ import {
   TextField,
   Typography,
   Button,
+  Link,
   Chip,
   Card,
-  CardContent
+  CardContent,
+  Divider
 } from '@mui/material'
 import Header from 'components/Header'
 import { getDiscussedTimes, getOtherTopics, getSpeakersList, getTopicResult } from 'redux/modules/topicDetail/actions'
@@ -44,74 +46,78 @@ const TopicDetailsPage = () => {
   }
 
   const handleClickOtherTopic = (item) => {
-    history.replace(`/${item.ngram.replace(/"/g, '')}`)
+    history.push(`/${item.ngram.replace(/"/g, '')}`)
   }
 
   const handleClickSpeaker = (speaker) => () => {
-    history.replace(`/speaker/${speaker.replace(/"|\[|\]/g, '')}`)
+    history.push(`/speaker/${speaker.replace(/"|\[|\]/g, '')}`)
   }
 
   return (
-    <Box>
-      <Box sx={{ mb: 5 }}>
-        <Header value={id} />
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Box sx={{ width: '100%', maxWidth: '200px' }}>
-          <TextField size="small" />
-          <Typography>First Time discussed</Typography>
-          <TextField size="small" />
-          <Typography>Last Time discussed</Typography>
-          <Box>Ads</Box>
-          <Typography>Speakers:</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            {speakers && Object.keys(speakers).map((speaker, index) => (
-              <Button
-                key={index}
-                variant='contained'
-                sx={{ mb: 1 }}
-                onClick={handleClickSpeaker(speaker)}
-              >
-                {speaker.replace(/\[|\]|"/g, '')}
-              </Button>
+    <Box sx={{ height: '100%' }}>
+      <Card sx={{ mb: 2 }}>
+        <Box>
+          <Header value={id} />
+        </Box>
+      </Card>
+      <Card sx={{ height: '100%' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: 4, height:'100%'}}>
+          <Box sx={{ width: '100%', maxWidth: '200px', mr: 4 }}>
+            <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', mb: 2 }}>
+              <TextField size="small" disabled sx={{ mb: 1 }} />
+              <Typography variant="medium">First Time discussed</Typography>
+            </Box>
+            <Box  sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+              <TextField size="small" disabled sx={{ mb: 1 }} />
+              <Typography variant="medium">Last Time discussed</Typography>
+            </Box>
+            <Box sx={{mb:10}}>Ads</Box>
+            <Typography variant='h5' sx={{mb:3}}>Speakers:</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              {speakers && Object.keys(speakers).map((speaker, index) => (
+                <Chip color='primary' variant="outlined" key={index} label={speaker.replace(/\[|\]|"/g, '')}
+                sx={{ mb: 2 }} onClick={handleClickSpeaker(speaker)} />
+              ))}
+            </Box>
+          </Box>
+          <Divider orientation="vertical" flexItem />
+          <Box sx={{ flexGrow: 1, px: 5 }}>
+            <Typography variant='h5' sx={{ mb: 2 }}>Results:</Typography>
+            <Box>
+              {topicResult && Object.values(topicResult).map((item, index) => (
+                <Card key={index} sx={{ mb: 2 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant='h5' component='div'>
+                      {item.publishedat.replace(/"/g, '')}
+                    </Typography>
+                    <Typography variant='body2' color='text.secondary'>
+                      Title: {item.channeltitle.replace(/"/g, '')}
+                    </Typography>
+                    <Typography variant='body2' color='text.secondary'>
+                      Party:
+                    </Typography>
+                    <Typography variant='body2' color='text.secondary'>
+                      Link: <Link href={item.url.replace(/"/g, '')}> {item.url.replace(/"/g, '')} </Link>
+                    </Typography>
+                    <Typography variant='body2' color='text.secondary'>
+                      Duration: {item.duration.replace(/"/g, '')} mins
+                    </Typography>
+                  </CardContent>
+                </Card>
+              ))}
+            </Box>
+          </Box>
+          <Divider orientation="vertical" flexItem />
+          <Box sx={{ maxWidth: '200px', width: '60%', display: 'flex', flexDirection: 'column', ml: 4 }}>
+            <Typography sx={{ mb: 2 }}>Other Topics Discussed...</Typography>
+            {otherTopics && Object.values(otherTopics).map((item, index) => (
+              <Chip color='primary' variant="outlined" key={index} label={item.ngram.replace(/"/g, '')}
+                sx={{ mb: 2 }} onClick={() => handleClickOtherTopic(item)} />
             ))}
+            <Box>Ads</Box>
           </Box>
         </Box>
-        <Box sx={{ flexGrow: 1, px: 5 }}>
-          <Typography sx={{ mb: 2 }}>Results:</Typography>
-          <Box>
-            {topicResult && Object.values(topicResult).map((item, index) => (
-              <Card key={index} sx={{ mb: 2 }}>
-                <CardContent>
-                  <Typography gutterBottom variant='h5' component='div'>
-                    {item.publishedat.replace(/"/g, '')}
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Title: {item.channeltitle.replace(/"/g, '')}
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Party:
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Link: {item.url.replace(/"/g, '')}
-                  </Typography>
-                  <Typography variant='body2' color='text.secondary'>
-                    Duration: {item.duration.replace(/"/g, '')} mins
-                  </Typography>
-                </CardContent>
-              </Card>
-            ))}
-          </Box>
-        </Box>
-
-        <Box sx={{ maxWidth: '200px', width: '100%', display: 'flex', flexDirection: 'column' }}>
-          <Typography>Other Topics Discussed...</Typography>
-          {otherTopics && Object.values(otherTopics).map((item, index) => (
-            <Chip key={index} label={item.ngram.replace(/"/g, '')} sx={{ mb: 2 }} onClick={() => handleClickOtherTopic(item)} />
-          ))}
-          <Box>Ads</Box>
-        </Box>
-      </Box>
+      </Card>
     </Box>
   )
 }
